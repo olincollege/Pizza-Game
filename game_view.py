a285aee948755bcdbff9dc377ca63165d4db6b4b
coxlaurel <coxlaurel@gmail.com>
2024-04-17 13:20:02 -0400

    added classes to game view file

---
+++ game_view.py
@@ -0,0 +1,27 @@
+"""
+Classes for viewing the game.
+"""
+
+class HomeScreen():
+
+class Button(HomeScreen):
+
+class StartButton(Button):
+
+class InstructionButton(Button):
+
+class Kitchen():
+
+class Order():
+
+class Pizza():
+
+class Toppings():
+
+class Cheese(Toppings):
+
+class Sauce(Toppings):
+
+class Basil(Toppings):
+
+class Mushroom(Toppings):
