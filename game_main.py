f20ca17979a4dd8fb638dda4ea39f706b164f025
coxlaurel <coxlaurel@gmail.com>
2024-04-17 13:39:39 -0400

    fixed screen display line in main file

--- game_main.py
+++ game_main.py
@@ -2,10 +2,10 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode(412, 771)
+screen = pygame.display.set_mode((412, 771))
 
 run = True
-while run == True:
+while run is True:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
