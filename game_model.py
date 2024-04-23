406ffae0c17099013fa7e21d2e7ae61cb505939a
coxlaurel <coxlaurel@gmail.com>
2024-04-23 13:13:38 -0400

    added topping class to model

--- game_model.py
+++ game_model.py
@@ -140,7 +140,22 @@ class PizzaStatus:
         A function that returns the pizza's current position
         """
         return self._position
+    
 
+class Toppings:
+    """
+    A parent class.
+    """
+    def __init__(self):
+        self._fall_speed = 5
+
+class Cheese(Toppings):
+    """
+    A class to create an instance of Cheese topping.
+    """
+    def __init__(self):
+        super().__init__()
+        
 
 class TimerStatus:
     """
