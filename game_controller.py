674683eb52b6cae31050a2f85c6f7f38cdca42b1
olivia-04 <osmith123x@gmail.com>
2024-04-23 13:14:15 -0400

    Controller edits

---
+++ game_controller.py
@@ -0,0 +1,29 @@
+class button:
+    """
+    A class to define button functions within the game
+    Attributes:
+        Goes-to: a string of the name of the environment that the button leads to
+    """
+
+
+class arrow:
+    """
+    A class to define the function of a button press
+        direction: whether the left or right button was pressed
+        move-pizza: true or false depending if the player is in active play of the game
+    """
+
+    def __init__(self):
+        """
+        Initiate an instance of the arrow class
+        """
+
+    def direction(self, movement):
+        """
+        Detects the direction of the arrow button
+        """
+
+    def move_pizza(self):
+        """
+        A function to move the piza left or right based on arrow interaction
+        """
