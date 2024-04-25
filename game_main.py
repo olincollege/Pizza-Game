decde7ab9b4d261a0466803c81fd5341d2860857
Vishnu Eskew <vishnu.eskew@gmail.com>
2024-04-24 20:08:02 -0400

    Fix main so that kitchen and buttons are separate screens in game.

--- game_main.py
+++ game_main.py
@@ -1,5 +1,6 @@
 import pygame
 from game_model import Button, OrderStatus, Pizza, PizzaStatus
+import time
 
 pygame.init()
 
@@ -19,26 +20,36 @@ homescreen = pygame.image.load("assets/img/homescreen.png")
 start_button = Button(50, 600, "assets/img/StartButton.png", 1, screen)
 exit_button = Button(250, 600, "assets/img/ExitButton.png", 1, screen)
 
-
-# game loop
-run = True
-while run:
+button_run = True
+while button_run:
     screen.blit(homescreen, (0, -100))  # display homescreen img
-
     if start_button.draw():
-        screen.blit(pygame.image.load('assets/img/kitchen.png'), (0, 0)) # display kitchen
-        order = OrderStatus() # initialized order
-        print(order.order_dict)
-        pizza_sprite = Pizza()
-        pizza_stat = PizzaStatus()
-        print(pizza_stat._current_toppings)
+        button_run = False
     if exit_button.draw():
         pygame.quit()
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
+            button_run = False
 
-    pygame.display.flip()
+    pygame.display.update()
 
+
+print(1)
+
+# game loop
+game_run = True
+while game_run:
+    screen.blit(
+        pygame.image.load("assets/img/kitchen.png"), (0, 0)
+    )  # display kitchen
+    order = OrderStatus()  # initialized order
+    print(order.order_dict)
+    pizza_sprite = Pizza()
+    pizza_stat = PizzaStatus()
+    print(pizza_stat._current_toppings)
+    pygame.display.update()
+    time.sleep(1)
 pygame.quit()
+for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+        game_run = False
