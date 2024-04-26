1b5e09d26b602532cb9a70162012e6355884c954
coxlaurel <coxlaurel@gmail.com>
2024-04-26 14:47:41 -0400

    fixed view file and main

--- main.py
+++ main.py
@@ -13,15 +13,15 @@ pygame.display.set_caption(
     "Cloudy with a Chance of Pizza"
 )  # window display caption
 
-# clock = pygame.time.Clock()
-# running = True
+clock = pygame.time.Clock()
+running = True
 
-def menu():
+def menu(screen):
     """
     A function to display home screen and menu.
     """
-    start_button = gmo.Button(50, 600, "assets/img/StartButton.png", 1, SCREEN)
-    exit_button_1 = gmo.Button(250, 600, "assets/img/ExitButton.png", 1, SCREEN)
+    start_button = gmo.Button(50, 600, "assets/img/StartButton.png", 1, screen)
+    exit_button_1 = gmo.Button(250, 600, "assets/img/ExitButton.png", 1, screen)
 
     clock = pygame.time.Clock()
     running = True
@@ -30,7 +30,7 @@ def menu():
             if event.type == pygame.QUIT:
                 running = False
 
-        gv.HomeScreen(SCREEN)
+        gv.HomeScreen(screen)
         if start_button.draw():
             running = False
         if exit_button_1.draw():
@@ -39,7 +39,7 @@ def menu():
         pygame.display.update()
         clock.tick(60)  # limits FPS to 60
 
-def play():
+def play(screen):
     """
     A function to display Play window.
     """
@@ -50,16 +50,13 @@ def play():
             if event.type == pygame.QUIT:
                 running = False
 
-        gv.Kitchen(SCREEN)
+        gv.Kitchen(screen)
 
         pygame.display.flip()
         clock.tick(60)  # limits FPS to 60
 
-menu()
-play()
-
-
-
+menu(SCREEN)
+play(SCREEN)
 
 
 # while running:
@@ -69,11 +66,8 @@ play()
 #         if event.type == pygame.QUIT:
 #             running = False
 
-#     # fill the screen with a color to wipe away anything from last frame
-#     screen.fill((0,0,0))
-
 #     # RENDER YOUR GAME HERE
-#     gv.HomeScreen(screen)
+#     gv.Kitchen(SCREEN)
 
 #     # flip() the display to put your work on screen
 #     pygame.display.flip()
