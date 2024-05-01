5726535ae4ce661e1d684353eb0b2406f785f4d2
coxlaurel <coxlaurel@gmail.com>
2024-05-01 11:20:53 -0400

    unit test

--- test_game_view.py
+++ test_game_view.py
@@ -6,32 +6,33 @@ import pytest
 import pygame
 import game_view as gv
 
-def test_collision_length_less_then_zero():
+def test_collision_length_less_than_zero():
     """
     Check that function returns None type if collision doesn't occur.
 
     Returns:
         A boolean. True if no collision is accurately detected.
     """
-    screen = pygame.display.set_mode((100,100))
+    screen = pygame.display.set_mode((10,10))
     topping_group = pygame.sprite.Group()
     pizza = gv.Pizza(screen)
     collision = gv.Toppings().collide_pizza(pizza, topping_group)
 
     return collision is None
 
+
 @pytest.mark.parametrize(
     "func",
     [
-        test_collision_length_less_then_zero,
+        test_collision_length_less_than_zero,
     ],
 )
-def test_PizzaStatus(func):  # pylint: disable=invalid-name
+def test_Topping(func):  # pylint: disable=invalid-name
     """
-    Check that the PizzaStatus class in game_model is working as intended.
+    Check that the Topping class in game_view is working as intended.
 
     Args:
         func: A function that returns a boolean, used to test various
-    functionality of the PizzaStatus class.
+    functionality of the Topping class.
     """
     assert func()
